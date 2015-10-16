c496ecd5c0b2de193ccd1fbb5fad25e4a3aafe06
Brian Silvia <brian@brinkbit.com>
2015-10-16 09:46:04 -0400

    Stub get actions out and add failing tests

--- src/get.js
+++ src/get.js
@@ -1,3 +1,56 @@
-module.exports = function get() {
-    return Promise.resolve( 3 );
+'use strict';
+
+/* eslint no-unused-vars: 0 */
+
+// TODO: handle proper validation of the path and resource
+function handleValidation( path, resource ) {
+    let retval = true;
+
+    if ( path === '' ) {
+        retval = 'INVALID_RESOURCE_PATH';
+    } else if ( resource === '' ) {
+        retval = 'INVALID_RESOURCE';
+    }
+
+    return retval;
+}
+
+module.exports.get = function get( path, resource ) {
+    const isValid = handleValidation( path, resource );
+
+    if ( isValid !== true ) {
+        return Promise.resolve({ status: isValid });
+    }
+
+    return Promise.resolve({});
+};
+
+module.exports.search = function search( path, resource, query, sorting, recursive ) {
+    const isValid = handleValidation( path, resource );
+
+    if ( isValid !== true ) {
+        return Promise.resolve({ status: isValid });
+    }
+
+    return Promise.resolve({});
+};
+
+module.exports.inspect = function inspect( path, resource, fields ) {
+    const isValid = handleValidation( path, resource );
+
+    if ( isValid !== true ) {
+        return Promise.resolve({ status: isValid });
+    }
+
+    return Promise.resolve({});
+};
+
+module.exports.download = function handleDownload( path, resource ) {
+    const isValid = handleValidation( path, resource );
+
+    if ( isValid !== true ) {
+        return Promise.resolve({ status: isValid });
+    }
+
+    return Promise.resolve({});
 };
