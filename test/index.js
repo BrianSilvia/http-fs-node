5bb3dab64da87386f9019e3345ae65bda19e8aab
Brian Silvia <brian@brinkbit.com>
2015-11-11 01:03:34 -0500

    Finish and consolidate tests under test/index.js
    This module handles routing and data structure validation. More
    specific tests of the operations will occur inside the fs-s3-mongo
    module.

--- test/index.js
+++ test/index.js
@@ -1,57 +1,396 @@
 'use strict';
 
 /* eslint-env mocha */
+/* eslint no-unused-expressions: 0 */
 
 const chai = require( 'chai' );
 const expect = chai.expect;
 const chaiaspromised = require( 'chai-as-promised' );
+const sinon = require( 'sinon' );
 const sinonchai = require( 'sinon-chai' );
 
-const modules = require( '../src/index.js' );
+const index = require( '../src/index.js' );
+const get = require( '../src/get.js' );
+const post = require( '../src/post.js' );
+const put = require( '../src/put.js' );
+const destroy = require( '../src/delete.js' );
 
 chai.use( sinonchai );
 chai.use( chaiaspromised );
 
-describe( 'General module validation', () => {
-    it( 'should return a 404/does not exist object when given an invalid path', () => {
-        const path = 'invalid/path/here/';
-        const resource = 'validFile.txt';
+describe( 'GET Actions', () => {
+    describe( 'READ', () => {
+        it( 'should route to get.read() with a valid path+resource', () => {
+            const path = 'valid/path/here/';
+            const resource = 'goat.jpg';
+            const spy = sinon.spy( get, 'read' );
 
-        return expect( modules.handleRequest( 'GET', path + resource )).to.be.rejected
-            .and.eventually.deep.equal({
-                status: 404,
-                message: 'Resource does not exist.',
-            });
+            index.handleRequest( 'GET', path + resource );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource )).to.be.true;
+
+            get.read.restore();
+        });
     });
 
-    it( 'should return a 404/invalid path object when given an invalid resource', () => {
-        const path = 'valid/path/here/';
-        const resource = 'invalidFile.txt';
+    describe( 'SEARCH', () => {
+        it( 'should reject with 501/invalid parameters when parameters.query is missing', () => {
+            const path = 'valid/path/here/';
+            const resource = 'goat.jpg';
+            const data = {
+                action: 'search',
+                parameters: {
+                    noQuery: '*',
+                },
+            };
 
-        return expect( modules.handleRequest( 'GET', path + resource )).to.be.rejected
-            .and.eventually.deep.equal({
-                status: 404,
-                message: 'Invalid path.',
-            });
+            return expect( index.handleRequest( 'GET', path + resource, data )).to.be.rejected
+                .and.eventually.deep.equal({
+                    status: 501,
+                    message: 'Invalid parameters.',
+                });
+        });
+
+        it( 'should return a 415/invalid type object when given a resource instead of a directory', () => {
+            const path = 'valid/path/here/';
+            const resource = 'notADirectory.txt';
+            const data = {
+                action: 'search',
+                parameters: {
+                    query: '*',
+                },
+            };
+
+            return expect( index.handleRequest( 'GET', path + resource, data )).to.be.rejected
+                .and.eventually.deep.equal({
+                    status: 415,
+                    message: 'Invalid resource type.',
+                });
+        });
+
+        it( 'should route to get.search() with a valid path+resource and query', () => {
+            const path = 'valid/path/here/';
+            const resource = 'folder/';
+            const data = {
+                action: 'search',
+                parameters: {
+                    query: '*',
+                },
+            };
+            const spy = sinon.spy( get, 'search' );
+
+            index.handleRequest( 'GET', path + resource, data );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource, data )).to.be.true;
+
+            get.search.restore();
+        });
     });
 
-    it( 'should return a 415/invalid type object when given an invalid resource type', () => {
-        const path = 'valid/path/here/';
-        const resource = 'notADirectory.txt';
-        const data = {
-            action: 'search',
-            parameters: {
-                query: '*',
-            },
-        };
+    describe( 'INSPECT', () => {
+        it( 'should route to get.inspect() with a valid path+resource', () => {
+            const path = 'valid/path/here/';
+            const resource = 'test.txt';
+            const data = {
+                action: 'inspect',
+            };
+            const spy = sinon.spy( get, 'inspect' );
 
-        return expect( modules.handleRequest( 'GET', path + resource, data )).to.be.rejected
-            .and.eventually.deep.equal({
-                status: 415,
-                message: 'Invalid resource type.',
-            });
+            index.handleRequest( 'GET', path + resource, data );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource, data )).to.be.true;
+
+            get.inspect.restore();
+        });
+
+        it( 'should route to get.inspect() with a valid path+resource including any specified fields', () => {
+            const path = 'valid/path/here/';
+            const resource = 'test.txt';
+            const data = {
+                action: 'inspect',
+                fields: [ 'name', 'parent' ],
+            };
+            const spy = sinon.spy( get, 'inspect' );
+
+            index.handleRequest( 'GET', path + resource, data );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource, data )).to.be.true;
+
+            get.inspect.restore();
+        });
     });
 
+    describe( 'DOWNLOAD', () => {
+        it( 'should route to get.download() with a valid path+resource', () => {
+            const path = 'valid/path/here/';
+            const resource = 'validFile.txt';
+            const data = {
+                action: 'download',
+            };
+            const spy = sinon.spy( get, 'download' );
+
+            index.handleRequest( 'GET', path + resource, data );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource )).to.be.true;
+
+            get.download.restore();
+        });
+    });
+});
+
+describe( 'POST Actions', () => {
+    describe( 'CREATE', () => {
+        it( 'should reject with 501/invalid parameters when parameters.content is missing', () => {
+            const path = 'valid/path/here/';
+            const resource = 'goat.jpg';
+            const data = {
+                parameters: {
+                    noContent: '',
+                },
+            };
+
+            return expect( index.handleRequest( 'POST', path + resource, data )).to.be.rejected
+                .and.eventually.deep.equal({
+                    status: 501,
+                    message: 'Invalid parameters.',
+                });
+        });
+
+        it( 'should route to post.create() with a valid path+resource and content', () => {
+            const path = 'valid/path/here/';
+            const resource = 'goat.jpg';
+            const data = {
+                parameters: {
+                    content: 'this content string',
+                },
+            };
+            const spy = sinon.spy( post, 'create' );
+
+            index.handleRequest( 'POST', path + resource, data );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource, data )).to.be.true;
+
+            post.create.restore();
+        });
+    });
+
+    describe( 'BULK', () => {
+        it( 'should reject with 501/invalid parameters when parameters.resources is missing', () => {
+            const path = 'valid/path/here/';
+            const resource = 'uploadFolder/';
+            const data = {
+                action: 'bulk',
+                parameters: {
+                    noResources: [],
+                },
+            };
+
+            return expect( index.handleRequest( 'POST', path + resource, data )).to.be.rejected
+                .and.eventually.deep.equal({
+                    status: 501,
+                    message: 'Invalid parameters.',
+                });
+        });
+
+        it( 'should route to post.bulk() with a valid path+resource and resources', () => {
+            const path = 'valid/path/here/';
+            const resource = 'uploadFolder/';
+            const data = {
+                action: 'bulk',
+                parameters: {
+                    resources: {
+                        'another_cat_picture': 'raw image data',
+                        'the_best_cats/': null,
+                    },
+                },
+            };
+            const spy = sinon.spy( post, 'bulk' );
+
+            index.handleRequest( 'POST', path + resource, data );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource, data )).to.be.true;
+
+            post.bulk.restore();
+        });
+    });
+
+    describe( 'COPY', () => {
+        it( 'should reject with 501/invalid parameters when parameters.destination is missing', () => {
+            const path = 'valid/path/here/';
+            const resource = 'goat.jpg';
+            const data = {
+                action: 'copy',
+                parameters: {
+                    noDestination: [],
+                },
+            };
+
+            return expect( index.handleRequest( 'POST', path + resource, data )).to.be.rejected
+                .and.eventually.deep.equal({
+                    status: 501,
+                    message: 'Invalid parameters.',
+                });
+        });
+
+        it( 'should route to post.copy() with a valid path+resource and destination', () => {
+            const path = 'valid/path/here/';
+            const resource = 'goat.jpg';
+            const data = {
+                action: 'copy',
+                parameters: {
+                    destination: 'valid/path/there/',
+                },
+            };
+            const spy = sinon.spy( post, 'copy' );
+
+            index.handleRequest( 'POST', path + resource, data );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource, data )).to.be.true;
+
+            post.copy.restore();
+        });
+    });
+});
+
+describe( 'PUT Actions', () => {
+    describe( 'UPDATE', () => {
+        it( 'should reject with 501/invalid parameters when parameters.content is missing', () => {
+            const path = 'valid/path/here/';
+            const resource = 'goat.jpg';
+            const data = {
+                parameters: {
+                    noContent: '',
+                },
+            };
+
+            return expect( index.handleRequest( 'PUT', path + resource, data )).to.be.rejected
+                .and.eventually.deep.equal({
+                    status: 501,
+                    message: 'Invalid parameters.',
+                });
+        });
+
+        it( 'should route to put.update() with a valid path+resource and content', () => {
+            const path = 'valid/path/here/';
+            const resource = 'goat.jpg';
+            const data = {
+                parameters: {
+                    content: 'this content string',
+                },
+            };
+            const spy = sinon.spy( put, 'update' );
+
+            index.handleRequest( 'PUT', path + resource, data );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource, data )).to.be.true;
+
+            put.update.restore();
+        });
+    });
+
+    describe( 'MOVE', () => {
+        it( 'should reject with 501/invalid parameters when parameters.destination is missing', () => {
+            const path = 'valid/path/here/';
+            const resource = 'goat.jpg';
+            const data = {
+                action: 'move',
+                parameters: {
+                    noDestination: 'not/here/',
+                },
+            };
+
+            return expect( index.handleRequest( 'PUT', path + resource, data )).to.be.rejected
+                .and.eventually.deep.equal({
+                    status: 501,
+                    message: 'Invalid parameters.',
+                });
+        });
+
+        it( 'should route to put.move() with a valid path+resource and destination', () => {
+            const path = 'valid/path/here/';
+            const resource = 'goat.jpg';
+            const data = {
+                action: 'move',
+                parameters: {
+                    destination: 'valid/path/there/',
+                },
+            };
+            const spy = sinon.spy( put, 'move' );
+
+            index.handleRequest( 'PUT', path + resource, data );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource, data )).to.be.true;
+
+            put.move.restore();
+        });
+    });
+
+    describe( 'RENAME', () => {
+        it( 'should reject with 501/invalid parameters when parameters.name is missing', () => {
+            const path = 'valid/path/here/';
+            const resource = 'goat.jpg';
+            const data = {
+                action: 'rename',
+                parameters: {
+                    noName: 'noName',
+                },
+            };
+
+            return expect( index.handleRequest( 'PUT', path + resource, data )).to.be.rejected
+                .and.eventually.deep.equal({
+                    status: 501,
+                    message: 'Invalid parameters.',
+                });
+        });
+
+        it( 'should route to put.rename() with a valid path+resource and destination', () => {
+            const path = 'valid/path/here/';
+            const resource = 'goat.jpg';
+            const data = {
+                action: 'rename',
+                parameters: {
+                    name: 'billyGoat.jpg',
+                },
+            };
+            const spy = sinon.spy( put, 'rename' );
+
+            index.handleRequest( 'PUT', path + resource, data );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource, data )).to.be.true;
+
+            put.rename.restore();
+        });
+    });
+});
+
+describe( 'DELETE actions', () => {
+    describe( 'DESTROY', () => {
+        it( 'should route to destroy.destroy() with a valid path+resource', () => {
+            const path = 'valid/path/here/';
+            const resource = 'goat.jpg';
+
+            const spy = sinon.spy( destroy, 'destroy' );
+
+            index.handleRequest( 'DELETE', path + resource );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource )).to.be.true;
+        });
+    });
+});
+
+describe( 'Top level routing', () => {
     it( 'should return a 501/invalid action object when given an invalid action', () => {
         const path = 'valid/path/here/';
         const resource = 'test.txt';
@@ -59,42 +398,10 @@ describe( 'General module validation', () => {
             action: 'invalidAction',
         };
 
-        return expect( modules.handleRequest( 'GET', path + resource, data )).to.be.rejected
+        return expect( index.handleRequest( 'GET', path + resource, data )).to.be.rejected
             .and.eventually.deep.equal({
                 status: 501,
                 message: 'Invalid action.',
             });
-    });
-
-    it( 'should return a 501/invalid parameters object when given an invalid set of parameters', () => {
-        const path = 'valid/path/here/';
-        const resource = 'validDirectory/';
-        const data = {
-            action: 'search',
-            parameters: {
-                queryWrong: '*',
-            },
-        };
-
-        return expect( modules.handleRequest( 'GET', path + resource, data )).to.be.rejected
-            .and.eventually.deep.equal({
-                status: 501,
-                message: 'Invalid parameters.',
-            });
-    });
-
-    it( 'should pass validation when given an appropriate path and resource', () => {
-        const path = 'valid/path/here/';
-        const resource = 'validDirectory/';
-        const data = {
-            action: 'search',
-            parameters: {
-                query: '*',
-            },
-        };
-
-        return expect( modules.handleRequest( 'GET', path + resource, data )).to.be.fulfilled
-            .and.eventually.have.property( 'data' )
-            .to.be.instanceof( Array );
     });
 });
