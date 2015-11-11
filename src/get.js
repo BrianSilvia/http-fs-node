14efbc2ea37694a2de0eceebb191f5787f929e56
Brian Silvia <brian@brinkbit.com>
2015-11-11 00:53:49 -0500

    Restub out action src files

--- src/get.js
+++ src/get.js
@@ -5,23 +5,23 @@
 module.exports.read = function read( fullPath ) {
     // TODO: Hit w3 bucket and retrieve
 
-    return Promise.resolve({ status: 'not yet implemented' });
+    return Promise.reject({ code: 501, message: 'Not implemented.' });
 };
 
 module.exports.search = function search( fullPath, data ) {
     // TODO: Hit mongo and retrieve folder contents
 
-    return Promise.resolve({ status: 'not yet implemented' });
+    return Promise.reject({ code: 501, message: 'Not implemented.' });
 };
 
 module.exports.inspect = function inspect( fullPath, data ) {
     // TODO: Hit mongo and retrieve metadata for the resource
 
-    return Promise.resolve({ status: 'not yet implemented' });
+    return Promise.reject({ code: 501, message: 'Not implemented.' });
 };
 
 module.exports.download = function handleDownload( fullPath ) {
     // TODO: Hit w3 bucket and retrieve, then compress
 
-    return Promise.resolve({ status: 'not yet implemented' });
+    return Promise.reject({ code: 501, message: 'Not implemented.' });
 };
