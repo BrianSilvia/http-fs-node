cb6aa7227f2812ca5c229dcb1819b004480fc0c5
Brian Silvia <brian@brinkbit.com>
2015-12-22 01:31:11 -0500

    Add tests for ingested fs-s3-mongo module

--- test/index.js
+++ test/index.js
@@ -3,6 +3,9 @@
 /* eslint-env mocha */
 /* eslint no-unused-expressions: 0 */
 
+// TODO: add tests for normalized flags
+// TODO: update fsS3Mongo functions with correct params
+
 const chai = require( 'chai' );
 const expect = chai.expect;
 const chaiaspromised = require( 'chai-as-promised' );
@@ -15,22 +18,46 @@ const post = require( '../src/post.js' );
 const put = require( '../src/put.js' );
 const destroy = require( '../src/delete.js' );
 
+// TODO: require module when it's ingested instead of this
+const fsS3Mongo = {
+    read: function read() {},
+    search: function search() {},
+    inspect: function inspect() {},
+    write: function write() {},
+    update: function update() {},
+    move: function move() {},
+    rename: function rename() {},
+    copy: function copy() {},
+    _destroy: function _destroy() {},
+};
+
 chai.use( sinonchai );
 chai.use( chaiaspromised );
 
-describe( 'GET Actions', () => {
+describe( 'GET Actions: ', () => {
     describe( 'READ', () => {
-        it( 'should route to get.read() with a valid path+resource', () => {
+        it( 'should route to index.read() with a valid path', () => {
             const path = 'valid/path/here/';
-            const resource = 'goat.jpg';
             const spy = sinon.spy( get, 'read' );
 
-            index.handleRequest( 'GET', path + resource );
+            index.handleRequest( 'GET', path );
 
             expect( spy.calledOnce ).to.be.true;
-            expect( spy.calledWithExactly( path + resource )).to.be.true;
+            expect( spy.calledWithExactly( path )).to.be.true;
 
             get.read.restore();
+        });
+
+        it( 'should ultimately route to fsS3Mongo.read() with a valid path', () => {
+            const path = 'valid/path/here/';
+            const spy = sinon.spy( fsS3Mongo, 'read' );
+
+            index.handleRequest( 'GET', path );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path )).to.be.true;
+
+            fsS3Mongo.read.restore();
         });
     });
 
@@ -69,7 +96,7 @@ describe( 'GET Actions', () => {
                 });
         });
 
-        it( 'should route to get.search() with a valid path+resource and query', () => {
+        it( 'should route to get.search() with a valid path, resource and query parameter', () => {
             const path = 'valid/path/here/';
             const resource = 'folder/';
             const data = {
@@ -87,10 +114,29 @@ describe( 'GET Actions', () => {
 
             get.search.restore();
         });
+
+        it( 'should ultimately route to fsS3Mongo.search() with a valid path, resource and query parameter', () => {
+            const path = 'valid/path/here/';
+            const resource = 'folder/';
+            const data = {
+                action: 'search',
+                parameters: {
+                    query: '*',
+                },
+            };
+            const spy = sinon.spy( fsS3Mongo, 'search' );
+
+            index.handleRequest( 'GET', path + resource, data );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource, data )).to.be.true;
+
+            fsS3Mongo.search.restore();
+        });
     });
 
     describe( 'INSPECT', () => {
-        it( 'should route to get.inspect() with a valid path+resource', () => {
+        it( 'should route to get.inspect() with a valid path and resource', () => {
             const path = 'valid/path/here/';
             const resource = 'test.txt';
             const data = {
@@ -106,7 +152,7 @@ describe( 'GET Actions', () => {
             get.inspect.restore();
         });
 
-        it( 'should route to get.inspect() with a valid path+resource including any specified fields', () => {
+        it( 'should route to get.inspect() with a valid path and resource including any specified fields', () => {
             const path = 'valid/path/here/';
             const resource = 'test.txt';
             const data = {
@@ -122,10 +168,43 @@ describe( 'GET Actions', () => {
 
             get.inspect.restore();
         });
+
+        it( 'should ultimately route to fsS3Mongo.inspect() with a valid path and resource', () => {
+            const path = 'valid/path/here/';
+            const resource = 'test.txt';
+            const data = {
+                action: 'inspect',
+            };
+            const spy = sinon.spy( fsS3Mongo, 'inspect' );
+
+            index.handleRequest( 'GET', path + resource, data );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource, '*' )).to.be.true;
+
+            fsS3Mongo.inspect.restore();
+        });
+
+        it( 'should ultimately route to fsS3Mongo.inspect() with a valid path and resource including any specified fields', () => {
+            const path = 'valid/path/here/';
+            const resource = 'test.txt';
+            const data = {
+                action: 'inspect',
+                fields: [ 'name', 'parent' ],
+            };
+            const spy = sinon.spy( fsS3Mongo, 'inspect' );
+
+            index.handleRequest( 'GET', path + resource, data );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource, data.fields )).to.be.true;
+
+            fsS3Mongo.inspect.restore();
+        });
     });
 
     describe( 'DOWNLOAD', () => {
-        it( 'should route to get.download() with a valid path+resource', () => {
+        it( 'should route to get.download() with a valid path and resource', () => {
             const path = 'valid/path/here/';
             const resource = 'validFile.txt';
             const data = {
@@ -139,6 +218,22 @@ describe( 'GET Actions', () => {
             expect( spy.calledWithExactly( path + resource )).to.be.true;
 
             get.download.restore();
+        });
+
+        it( 'should ultimately route to fsS3Mongo.read() with a valid path and resource, and compression flag', () => {
+            const path = 'valid/path/here/';
+            const resource = 'validFile.txt';
+            const data = {
+                action: 'download',
+            };
+            const spy = sinon.spy( fsS3Mongo, 'read' );
+
+            index.handleRequest( 'GET', path + resource, data );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource, 'zip' )).to.be.true;
+
+            fsS3Mongo.read.restore();
         });
     });
 });
@@ -161,7 +256,7 @@ describe( 'POST Actions', () => {
                 });
         });
 
-        it( 'should route to post.create() with a valid path+resource and content', () => {
+        it( 'should route to post.create() with a valid path and resource, and content', () => {
             const path = 'valid/path/here/';
             const resource = 'goat.jpg';
             const data = {
@@ -177,6 +272,24 @@ describe( 'POST Actions', () => {
             expect( spy.calledWithExactly( path + resource, data )).to.be.true;
 
             post.create.restore();
+        });
+
+        it( 'should ultimately route to fsS3Mongo.create() with a valid path and resource, and content', () => {
+            const path = 'valid/path/here/';
+            const resource = 'goat.jpg';
+            const data = {
+                parameters: {
+                    content: 'this content string',
+                },
+            };
+            const spy = sinon.spy( fsS3Mongo, 'write' );
+
+            index.handleRequest( 'POST', path + resource, data );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource, data.parameters.content )).to.be.true;
+
+            fsS3Mongo.write.restore();
         });
     });
 
@@ -198,7 +311,7 @@ describe( 'POST Actions', () => {
                 });
         });
 
-        it( 'should route to post.bulk() with a valid path+resource and resources', () => {
+        it( 'should route to post.bulk() with a valid path and resource, and resources', () => {
             const path = 'valid/path/here/';
             const resource = 'uploadFolder/';
             const data = {
@@ -218,6 +331,52 @@ describe( 'POST Actions', () => {
             expect( spy.calledWithExactly( path + resource, data )).to.be.true;
 
             post.bulk.restore();
+        });
+
+        // TODO: Add additional validation tests for bulk()
+
+        it( 'should ultimately route to fsS3Mongo.write() with a valid path and resource, and resources', () => {
+            const path = 'valid/path/here/';
+            const resource = 'uploadFolder/';
+            const data = {
+                action: 'bulk',
+                parameters: {
+                    resources: {
+                        'another_cat_picture': 'raw image data',
+                    },
+                },
+            };
+            const spy = sinon.spy( fsS3Mongo, 'write' );
+
+            index.handleRequest( 'POST', path + resource, data );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource, { 'another_cat_picture': 'raw image data' })).to.be.true;
+
+            fsS3Mongo.write.restore();
+        });
+
+        it( 'should call into fsS3Mongo.write() once per resource, with a valid path and resource, and resources', () => {
+            const path = 'valid/path/here/';
+            const resource = 'uploadFolder/';
+            const data = {
+                action: 'bulk',
+                parameters: {
+                    resources: {
+                        'another_cat_picture': 'raw image data',
+                        'the_best_cats/': null,
+                    },
+                },
+            };
+            const spy = sinon.spy( fsS3Mongo, 'write' );
+
+            index.handleRequest( 'POST', path + resource, data );
+
+            expect( spy.calledTwice ).to.be.true;
+            expect( spy[0].calledWithExactly( path + resource, { 'another_cat_picture': 'raw image data' })).to.be.true;
+            expect( spy[1].calledWithExactly( path + resource, { 'the_best_cats/': null })).to.be.true;
+
+            fsS3Mongo.write.restore();
         });
     });
 
@@ -239,7 +398,7 @@ describe( 'POST Actions', () => {
                 });
         });
 
-        it( 'should route to post.copy() with a valid path+resource and destination', () => {
+        it( 'should route to post.copy() with a valid path and resource, and destination', () => {
             const path = 'valid/path/here/';
             const resource = 'goat.jpg';
             const data = {
@@ -256,6 +415,25 @@ describe( 'POST Actions', () => {
             expect( spy.calledWithExactly( path + resource, data )).to.be.true;
 
             post.copy.restore();
+        });
+
+        it( 'should ultimately route to fsS3Mongo.copy() with a valid path and resource, and destination', () => {
+            const path = 'valid/path/here/';
+            const resource = 'goat.jpg';
+            const data = {
+                action: 'copy',
+                parameters: {
+                    destination: 'valid/path/there/',
+                },
+            };
+            const spy = sinon.spy( fsS3Mongo, 'copy' );
+
+            index.handleRequest( 'POST', path + resource, data );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource, data.parameters.destination )).to.be.true;
+
+            fsS3Mongo.copy.restore();
         });
     });
 });
@@ -278,7 +456,7 @@ describe( 'PUT Actions', () => {
                 });
         });
 
-        it( 'should route to put.update() with a valid path+resource and content', () => {
+        it( 'should route to put.update() with a valid path and resource, and content', () => {
             const path = 'valid/path/here/';
             const resource = 'goat.jpg';
             const data = {
@@ -294,6 +472,24 @@ describe( 'PUT Actions', () => {
             expect( spy.calledWithExactly( path + resource, data )).to.be.true;
 
             put.update.restore();
+        });
+
+        it( 'should ultimately route to fsS3Mongo.update() with a valid path and resource, and content', () => {
+            const path = 'valid/path/here/';
+            const resource = 'goat.jpg';
+            const data = {
+                parameters: {
+                    content: 'this content string',
+                },
+            };
+            const spy = sinon.spy( fsS3Mongo, 'update' );
+
+            index.handleRequest( 'PUT', path + resource, data );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource, data.parameters.content )).to.be.true;
+
+            fsS3Mongo.update.restore();
         });
     });
 
@@ -315,7 +511,7 @@ describe( 'PUT Actions', () => {
                 });
         });
 
-        it( 'should route to put.move() with a valid path+resource and destination', () => {
+        it( 'should route to put.move() with a valid path and resource, and destination', () => {
             const path = 'valid/path/here/';
             const resource = 'goat.jpg';
             const data = {
@@ -332,6 +528,25 @@ describe( 'PUT Actions', () => {
             expect( spy.calledWithExactly( path + resource, data )).to.be.true;
 
             put.move.restore();
+        });
+
+        it( 'should ultimately route to fsS3Mongo.move() with a valid path and resource, and destination', () => {
+            const path = 'valid/path/here/';
+            const resource = 'goat.jpg';
+            const data = {
+                action: 'move',
+                parameters: {
+                    destination: 'valid/path/there/',
+                },
+            };
+            const spy = sinon.spy( fsS3Mongo, 'move' );
+
+            index.handleRequest( 'PUT', path + resource, data );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource, data.parameters.destination )).to.be.true;
+
+            fsS3Mongo.move.restore();
         });
     });
 
@@ -353,7 +568,7 @@ describe( 'PUT Actions', () => {
                 });
         });
 
-        it( 'should route to put.rename() with a valid path+resource and destination', () => {
+        it( 'should route to put.rename() with a valid path and resource, and destination', () => {
             const path = 'valid/path/here/';
             const resource = 'goat.jpg';
             const data = {
@@ -371,12 +586,31 @@ describe( 'PUT Actions', () => {
 
             put.rename.restore();
         });
+
+        it( 'should ultimately route to fsS3Mongo.rename() with a valid path and resource, and destination', () => {
+            const path = 'valid/path/here/';
+            const resource = 'goat.jpg';
+            const data = {
+                action: 'rename',
+                parameters: {
+                    name: 'billyGoat.jpg',
+                },
+            };
+            const spy = sinon.spy( fsS3Mongo, 'rename' );
+
+            index.handleRequest( 'PUT', path + resource, data );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource, data.parameters.name )).to.be.true;
+
+            fsS3Mongo.rename.restore();
+        });
     });
 });
 
 describe( 'DELETE actions', () => {
     describe( 'DESTROY', () => {
-        it( 'should route to destroy.destroy() with a valid path+resource', () => {
+        it( 'should route to destroy.destroy() with a valid path and resource', () => {
             const path = 'valid/path/here/';
             const resource = 'goat.jpg';
 
@@ -386,11 +620,51 @@ describe( 'DELETE actions', () => {
 
             expect( spy.calledOnce ).to.be.true;
             expect( spy.calledWithExactly( path + resource )).to.be.true;
+
+            destroy.destroy.restore();
+        });
+
+        it( 'should ultimately route to fsS3Mongo.destroy() with a valid path and resource', () => {
+            const path = 'valid/path/here/';
+            const resource = 'goat.jpg';
+
+            const spy = sinon.spy( fsS3Mongo, '_destroy' );
+
+            index.handleRequest( 'DELETE', path + resource );
+
+            expect( spy.calledOnce ).to.be.true;
+            expect( spy.calledWithExactly( path + resource )).to.be.true;
+
+            fsS3Mongo._destroy.restore();
         });
     });
 });
 
 describe( 'Top level routing', () => {
+    it( 'should return a 404/invalid path or resource with an empty path', () => {
+        const path = '';
+
+        index.handleRequest( 'GET', path );
+
+        return expect( index.handleRequest( 'GET', path )).to.be.rejected
+            .and.eventually.deep.equal({
+                status: 404,
+                message: 'Invalid path or resource.',
+            });
+    });
+
+    it( 'should return a 404/invalid path or resource with a null path', () => {
+        const path = null;
+
+        index.handleRequest( 'GET', path );
+
+        return expect( index.handleRequest( 'GET', path )).to.be.rejected
+            .and.eventually.deep.equal({
+                status: 404,
+                message: 'Invalid path or resource.',
+            });
+    });
+
     it( 'should return a 501/invalid action object when given an invalid action', () => {
         const path = 'valid/path/here/';
         const resource = 'test.txt';
